4619d2c0095635e338b7becdecce31e1e7af72ca
Charles You <cyou77@berkeley.edu>
2018-02-22 17:50:20 -0800

    Done

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,21 +4,22 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = nickname
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    name[0..3]
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    Time.zone.now.year - age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    name + " " + age
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'age', to: 'pages#age'
+  get 'me', to: 'pages#me'
+  post 'root', to: 'pages#stringify', as: 'stringify'
+  put 'age', to: 'pages#person', as: 'person'
 end
